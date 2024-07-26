087bd6e567c6f4f37e2b5609e574240fd1b8ad6c
Tuguldur Baatarsuren <btuguldur397@gmail.com>
2024-07-26 07:52:13 +0000

    11

--- internship_log/api/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import InternshipEntry
-
-class InternshipEntrySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = InternshipEntry
-        fields = '__all__'
